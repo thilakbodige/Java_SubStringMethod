ae33e3dfd331a66e9d1137d96293d6d7234ae370
Thilak Bodige <104753214+thilakbodige@users.noreply.github.com>
2025-03-14 20:06:49 +0530

    Add files via upload

---
+++ Sub_Str.java
@@ -0,0 +1,16 @@
+package com.SubStringMethod;
+
+import java.util.Scanner;
+
+public class Sub_Str
+{
+	public static void main(String[] args) 
+	{
+	 Scanner sc=new Scanner(System.in);
+     System.out.println("Enter the String:");	
+     String s=sc.next();
+     System.out.println(s.substring(2));
+	}
+
+}
+
