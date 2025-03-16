9e5dda46d3499872aa9439fc04985884aeaadf70
Thilak Bodige <104753214+thilakbodige@users.noreply.github.com>
2025-03-16 18:52:07 +0530

    Add files via upload

---
+++ IntegerNum_Str.java
@@ -0,0 +1,11 @@
+package com.SubStringMethod;
+
+public class IntegerNum_Str 
+{
+	public static void main(String[] args)
+	{
+    String s="814";
+    int a=Integer.parseInt(s);
+    System.out.println(a%2==0);
+	}
+}
