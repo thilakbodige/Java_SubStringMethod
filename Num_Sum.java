84d93928eefe86b5b4b87280b2336755c4129a5f
Thilak Bodige <104753214+thilakbodige@users.noreply.github.com>
2025-03-16 18:53:59 +0530

    Add files via upload

---
+++ Num_Sum.java
@@ -0,0 +1,34 @@
+package com.SubStringMethod;
+import java.util.*;
+public class Num_Sum 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the String : ");
+		String s=sc.next();
+		int sum=0;
+		for (int i=0;i<s.length();i++) 
+		{
+			String temp="";
+			for (int j=i;j<s.length();j++) 
+			{
+				char ch=s.charAt(j);
+				if (ch>='0'&&ch<='9') 
+				{
+					temp=temp+ch;
+				} 
+				else 
+				{
+					break;
+				}
+			}
+			if (!(temp.equals(""))) 
+			{
+				int n=Integer.parseInt(temp);
+				sum=sum+n;
+			}
+		}
+		System.out.println("The Sum of Numbers inside the String " +s+" is "+sum);
+	}
+}
